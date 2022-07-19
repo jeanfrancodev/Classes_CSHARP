770f829ebb8203f1c7ffdd3cc83488fad6f2ad68
Jean Franco <93026071+jeanfrancodev@users.noreply.github.com>
2022-07-19 17:34:55 -0300

    Add files via upload

---
+++ atividade_CLASSE/Program.cs
@@ -0,0 +1,22 @@
+﻿using atividade_CLASSE.src;
+using System;
+
+namespace atividade_CLASSE { 
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ChampLeagueOfLegends Garen = new ChampLeagueOfLegends(Categoria.Tanque, "Garen", 616, 0, 57.88);
+            ChampLeagueOfLegends Lucian = new ChampLeagueOfLegends(Categoria.Atirador, "Lucian", 390,0, 46);
+            ChampLeagueOfLegends Anivia = new ChampLeagueOfLegends(Categoria.Mago, "Anivia", 467, 60, 51.37);
+            ChampLeagueOfLegends Janna = new ChampLeagueOfLegends(Categoria.Suporte, "Janna", 356, 50, 49);
+            ChampLeagueOfLegends Jax = new ChampLeagueOfLegends(Categoria.Lutador, "Jax", 463, 40, 56.3);
+
+            Console.WriteLine(Garen.ToString());
+            Console.WriteLine(Lucian.ToString());
+            Console.WriteLine(Anivia.ToString());
+            Console.WriteLine(Janna.ToString());
+            Console.WriteLine(Jax.ToString());
+        }
+    }
+}
---
+++ atividade_CLASSE/src/ChampLeagueOfLegends.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace atividade_CLASSE.src
+{
+    public class ChampLeagueOfLegends
+    {
+        Categoria Posicao { get; set; }
+        string Nome { get; set; }
+
+        int Vida { get; set; }
+
+        double PoderDeHabilidade { get; set; }
+
+        double Ataque { get; set; }
+
+        public ChampLeagueOfLegends(Categoria posicao, string nome, int vida, double poderDeHabilidade, double ataque)
+        {
+            Posicao = posicao;
+            Nome = nome;
+            Vida = vida;
+            PoderDeHabilidade = poderDeHabilidade;
+            Ataque = ataque;
+        }
+        public override string ToString()
+        {
+            return $"\n Nome do Campeão: {Nome}" +
+                   $"\n Tipo: {Posicao}" +
+                   $"\n Vida inicial: {Vida}" +
+                   $"\n AP inicial: {PoderDeHabilidade}" +
+                   $"\n Dano inicial: {Ataque}";
+        }
+    }
+
+    public enum Categoria
+    {
+        Atirador,
+        Tanque,
+        Mago,
+        Suporte,
+        Lutador,
+    }
+
+
+}
